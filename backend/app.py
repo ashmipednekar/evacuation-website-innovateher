d1e718cd6605e95f125c5ea2f1f5223fbc1e875c
Ashmi Pednekar <ashmipednekar@gmail.com>
2025-01-25 17:23:14 -0500

    minor changes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from collections.abc import MutableMapping
 from flask_pymongo import PyMongo
+import gridfs
 
 app = Flask(__name__)
 
