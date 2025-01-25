310e8f9dbda95f68ccb9b83e99ec4618f1dd64cc
Ashmi Pednekar <ashmipednekar@gmail.com>
2025-01-25 12:54:58 -0500

    base files

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+from collections.abc import MutableMapping
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+# MongoDB Configuration
+#my pass: 59Dk0F56
+app.config["MONGO_URI"] = "mongodb+srv://ashmipednekar:59Dk0F56@gumpack-cluster.u6mjj.mongodb.net/?retryWrites=true&w=majority&appName=gumpack-cluster"
+mongo = PyMongo(app)
+
+# Home Route
+@app.route("/")
+def index():
+    buildings = mongo.db.buildings.find()  # Fetch buildings from MongoDB
+    return render_template("index.html", buildings=buildings)
+
+# Add Building
+@app.route("/add", methods=["POST"])
+def add_building():
+    building_name = request.form.get("name")
+    address = request.form.get("address")
+    floors = request.form.get("floors")
+
+    if building_name and address and floors:
+        mongo.db.buildings.insert_one({
+            "name": building_name,
+            "address": address,
+            "floors": int(floors)
+        })
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
